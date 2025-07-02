cc58e1f49481f9258db74af39ae661cd86711d44
nosbos2323 <medpikora@gmail.com>
2025-07-02 16:14:30 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { Routes, Route } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
+import routes from "tempo-routes";
+import Home from "./components/home";
+import LoginPage from "./components/auth/LoginPage";
+import PaymentsPage from "./components/payments/PaymentsPage";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background">
+      {/* For the tempo routes */}
+      {import.meta.env.VITE_TEMPO && useRoutes(routes)}
+
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/payments" element={<PaymentsPage />} />
+        <Route path="/google-signin" element={<LoginPage />} />
+        <Route path="/signin" element={<LoginPage />} />
+
+        {/* Add this before the catchall route */}
+        {import.meta.env.VITE_TEMPO && <Route path="/tempobook/*" />}
+
+        {/* Redirect root to login */}
+        <Route path="/" element={<LoginPage />} />
+        <Route path="*" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
